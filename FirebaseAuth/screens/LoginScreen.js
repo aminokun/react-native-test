56d8920e668efd61fc34680e5ba1f3a4b61e99fa
amine <amine.dbh99@gmail.com>
2022-11-30 13:57:11 +0100

    comm

--- FirebaseAuth/screens/LoginScreen.js
+++ FirebaseAuth/screens/LoginScreen.js
@@ -1,8 +1,46 @@
-import { KeyboardAvoidingView, PermissionsAndroid, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, PermissionsAndroid, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, } from 'react-native'
+import React, {useEffect, useState} from 'react'
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user ) {
+        navigation.navigate("Home")
+      }
+    })
+    return unsubscribe
+  }, [])
+
+
+  const handleSignUp = () => {
+    auth
+    .createUserWithEmailAndPassword(email,password)
+    .then(userCredentials => { 
+      const user = userCredentials.user;
+      console.log('Registered with: ', user.email);
+    })
+    .catch(error => alert(error.message))
+  }
+
+  const handleLogin = () => {
+    auth
+    .signInWithEmailAndPassword(email, password)
+    .then(userCredentials => { 
+      const user = userCredentials.user;
+      console.log('Logged in with: ', user.email);
+    })
+    .catch(error => alert(error.message))
+  }
+
+
   return (
     <KeyboardAvoidingView
     style={styles.container}
@@ -10,15 +48,15 @@ const LoginScreen = () => {
     >
       <View style = { styles.inputContainer}>
         <TextInput
-            placeholder="email"
-            // value={""}
-            // onChangeText={ text =>}
+            placeholder="Email"
+            value={email}
+            onChangeText={ text => setEmail(text)}
             style={styles.input}
         />
         <TextInput
-            placeholder="password"
-            // value={""}
-            // onChangeText={ text =>}
+            placeholder="Password"
+            value={password}
+            onChangeText={ text => setPassword(text)}
             style={styles.input}
             secureTextEntry
         />
@@ -26,10 +64,17 @@ const LoginScreen = () => {
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity
-        onPress={() => { }}
+        onPress={handleLogin}
         style={styles.button}
         >
-        <Text>Login</Text>
+        <Text style={styles.buttonText}>Login</Text>
+
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={handleSignUp}
+        style={[styles.button, styles.buttonOutline]}
+        >
+        <Text style={styles.buttonOutlineText}>Register</Text>
 
         </TouchableOpacity>
       </View>
@@ -45,5 +90,49 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
 
-    }
+    },
+    inputContainer: {
+      width: '80%',
+    },
+    input: {
+      backgroundColor: 'white',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 75,
+      marginTop: 5,
+    },
+    buttonContainer: {
+      width: '60%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 40,
+    },
+    button: {
+      backgroundColor: '#28b4ee',
+      width: '100%',
+      padding: 15,
+      borderRadius: 67,
+      alignItems: 'center'
+
+    }, 
+    buttonOutline: {
+      backgroundColor: 'white',
+      marginTop: 5,
+      borderColor: '#28b4ee',
+      borderWidth: 2,
+
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: '700',
+      fontSize: 16,
+
+    },
+   
+    buttonOutlineText: {
+      color: '#28b4ee',
+      fontWeight: '700',
+      fontSize: 16,
+      
+    },
 })
